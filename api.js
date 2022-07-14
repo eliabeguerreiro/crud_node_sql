fe80278455fa41ab93b3201fb50a458b90ac3039
Eliabe G da Paz <eliabepaz.work@gmail.com>
2022-07-14 16:29:19 -0300

    verificação de token e tudo mais

--- api.js
+++ api.js
@@ -1,16 +1,17 @@
-//const express = require("express");
-const sql = require("mssql");
+const express = require("express");
+const app = express();
+
+const cors = require('cors'); 
+const router = express.Router();
+
+const jwt = require('jsonwebtoken');
+const SECRET = 'r2d2ph1rm1';
+
+//token deve ser inserido em todas as requisições
+
 var Produtos = require('./produtos');
 const dboperations = require("./dboperations");
-
-var express = require('express');
 var bodyParser = require('body-parser');
-var cors = require('cors'); 
-var app = express();
-var router = express.Router();
-
-
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -23,14 +24,67 @@ router.use((req,res,next)=>{
     next();
 });
 
+/*
 router.route('/produtos').get((req,res)=>{
 
+});
+*/
+
+//middleware de verificação
+function verifyJWT(req,res,next){
+    const token = req.headers['x-acess-token'];
+    jwt.verify(token, SECRET, (err, decoded) =>{
+        if(err) return res.status(401).end();
+    
+        req.userId = decoded.userId;
+        next();
+    })
+}
+
+router.route('/produtos/todos').get(verifyJWT, (req,res)=>{
+    console.log(res.userId + 'fez esta chamada!');
     dboperations.getProdutos().then(result => {
         //console.log(result);
         res.send(result);
     })
 
 });
+
+router.route('/login').post((req,res) =>{
+    if(req.body.user === 'hants' && req.body.password ==='hants12'){
+        const token = jwt.sign({userId: 1}, SECRET, {expiresIn: 1200});
+        return res.json({auth: true, token});;
+    }
+    res.status(401).end();
+});
+
+
+
+
+
+router.route('/produtos/:sku').get((req,res)=>{
+
+    dboperations.getProduto(req.params.sku).then(result => {
+
+        res.send(result);
+    })
+
+});
+
+/*
+INSERIR DADOS
+
+router.route('/produtos').post((req,res)=>{
+    let produto = {...req.body}
+
+    dboperations.addProduto(produto).then(result => {
+        res.status(201).result;
+    })
+
+});
+*/
+
+
 
 
 var port = process.env.PORT || 8090;
--- dboperations.js
+++ dboperations.js
@@ -1,3 +1,4 @@
+//Tutorial guia: https://www.youtube.com/watch?v=Uvy_BlgwfLI&ab_channel=Geek97 
 var config = require('./dbconfig');
 const sql = require('mssql');
 
@@ -6,7 +7,7 @@ const sql = require('mssql');
 async function getProdutos(){
     try{
         let pool = await sql.connect(config);
-        let products = await pool.request().query("select * from app_farma_produtos where sku < 5");
+        let products = await pool.request().query("SELECT * from app_farma_produtos");
         return products.recordsets;
 
     }
@@ -15,6 +16,43 @@ async function getProdutos(){
     }
 }
 
+async function getProduto(sku){
+    try{
+        let pool = await sql.connect(config);
+        let products = await pool.request()
+            .input('input_parameter', sql.Int, sku)
+            .query("SELECT * FROM app_farma_produtos WHERE sku = @input_parameter");
+        return products.recordsets;
+
+    }
+    catch (erro){
+        console.log(erro)
+    }
+}
+
+/*
+async function addProduto(produto){
+    try{
+        let poll = await sql.connect(config);
+        let insertProduct = await poll.request()
+            .input('Id', sql.Int, produto.Id)
+            .input('Title', sql.NVarChar, produto.Title)
+            .input('Quantity', sql.Int, produto.Quantity)
+            .input('Messsage', sql.NVarChar, produto.Message)
+            .input('City', sql.NVarChar, produto.City)
+            .execute('InsertProduto');
+        return insertProduct.recordsets;
+
+    }catch (erro){
+        console.log(erro)
+    }
+
+};
+*/
+
+
 module.exports ={
-    getProdutos : getProdutos
+    getProdutos : getProdutos,
+    getProduto : getProduto/*,
+    addProduto : addProduto*/
 }
